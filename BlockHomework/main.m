9f5466cc12b2726467c3d685d39805b61c1532fe
usernamenameuser <writeme@email.ua>
2017-04-13 15:16:12 +0300

    Initial Commit

---
+++ BlockHomework/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BlockHomework
+//
+//  Created by user on 13.04.17.
+//  Copyright © 2017 iPodium. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
